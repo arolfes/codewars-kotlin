57accce4406bb9e3decc7c8242c143b32114772b
Alex <arolfes@users.noreply.github.com>
2019-11-24 16:10:22 +0100

    best practice
    https://www.codewars.com/kata/moves-in-squared-strings-i/kotlin

--- src/main/kotlin/com/github/arolfes/codewars/Opstrings.kt
+++ src/main/kotlin/com/github/arolfes/codewars/Opstrings.kt
@@ -1,17 +1,14 @@
 package com.github.arolfes.codewars
 
-import java.util.stream.Collectors
-import kotlin.reflect.KFunction1
-
 /**
  * https://www.codewars.com/kata/moves-in-squared-strings-i/kotlin
  */
 object Opstrings {
 
-    fun vertMirror(strng: String): String = ( strng.split("\n").stream().map { t -> t.reversed() }.collect(Collectors.joining("\n")) )
+    fun vertMirror(strng: String) = strng.split("\n").map { it.reversed() }.joinToString("\n")
 
-    fun horMirror(strng: String): String = (strng.split("\n").reversed().joinToString("\n"))
+    fun horMirror(strng: String) = strng.split("\n").reversed().joinToString("\n")
 
-    fun oper(f: KFunction1<String, String>, s: String): String =  (f (s))
+    fun oper(f: (String) -> String , s: String): String = f(s)
 
 }
